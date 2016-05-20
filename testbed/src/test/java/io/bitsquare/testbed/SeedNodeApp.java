4a4a9f2d08814f30be4fd25e11a56b37247a94ab
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2016-05-20 11:01:32 +0200

    Example ``egrep`` command to select testbed log entries

--- testbed/src/test/java/io/bitsquare/testbed/SeedNodeApp.java
+++ testbed/src/test/java/io/bitsquare/testbed/SeedNodeApp.java
@@ -118,7 +118,14 @@ public class SeedNodeApp {
         return new NodeAddress(hostName, port);
     }
 
-    /** Print a timestamped testbed log entry to standard output. */
+    /** Print a timestamped testbed log entry to standard output.
+     *
+     * You can select these lines with a command like
+     * {@code egrep '^(ESC\[[0-9;m]+)?TB '}
+     * where {@code ESC} is the actual escape character (e.g Ctrl-V Esc in Bash).
+     * The reason for such complex command is that loggers may colorize output
+     * regardless of whether it is being sent to a pipe.
+     */
     private static void testLog(String format, Object... args) {
         System.out.println(
                 String.format("TB %s %s",
