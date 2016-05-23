21300a6406317c74eaa8cad85aa855642771990e
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2016-05-23 14:40:52 +0200

    Some documentation for testbed controller peer

--- testbed/src/test/java/io/bitsquare/testbed/ControllerPeerApp.java
+++ testbed/src/test/java/io/bitsquare/testbed/ControllerPeerApp.java
@@ -12,6 +12,16 @@ import java.util.concurrent.TimeUnit;
 
 import static com.google.common.base.Preconditions.checkState;
 
+/**
+ * Controller peer application for testbed experiments.
+ *
+ * You may run this class to create a controller peer on the regression test network
+ * which can contact other testbed peers with the help of a seed node
+ * and manage them for running a test.
+ *
+ * You must provide an argument with
+ * the {@code HOSTNAME:PORT} address of the seed node.
+ */
 public class ControllerPeerApp extends PeerApp {
     public static void main(String[] args) {
         if (args.length < 1) {
@@ -33,11 +43,13 @@ public class ControllerPeerApp extends PeerApp {
         super(seedAddr);
     }
 
+    /** Setup the sending and reception of messages. */
     void start() {
         broadcaster = new Broadcaster(peer.getNetworkNode(), peer.getPeerManager());
         UserThread.runPeriodically(this::broadcastHello, HELLO_INTERVAL_SECS, TimeUnit.SECONDS);
     }
 
+    /** Periodically announce public key so that other peers can send us direct messages. */
     private void broadcastHello() {
         final KeyRing keyRing = peer.getKeyRing();
         checkState(keyRing != null, "keyring missing in already bootstrapped node");
